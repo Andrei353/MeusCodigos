0de806eef03135a7554f4fd6bad809f4373a199a
Andrei353 <143633137+Andrei353@users.noreply.github.com>
2023-09-20 16:44:07 -0300

    Add files via upload

---
+++ OlaMundo.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>  //padrão de entrada e saida
+#include <stdlib.h> //biblioteca base
+
+int main(){//main é principal 
+//usando os dois main(), é chamado de função principal
+    printf("Olá Mundo!"); // printf é o mesmo de escreval
+    return 0;
+}
